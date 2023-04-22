0521ea875f06f1ae49b2e5a0c527743595c55e5a
adcostanzi <andres.costanzi@outlook.com>
2023-04-22 01:59:08 -0500

    home page html, css and javascript for w01 completed

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const year = new Date().getFullYear();
+document.querySelector("#year").innerHTML = year;
+
+let update = document.lastModified;
+console.log(update);
+document.querySelector("#last-update").innerHTML = update;
