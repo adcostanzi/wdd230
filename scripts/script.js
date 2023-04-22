05ea985bc6e994db0b45c8a69927f440db5301bd
adcostanzi <andres.costanzi@outlook.com>
2023-04-22 02:21:44 -0500

    log removed from js file

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,4 @@ const year = new Date().getFullYear();
 document.querySelector("#year").innerHTML = year;
 
 let update = document.lastModified;
-console.log(update);
 document.querySelector("#last-update").innerHTML = `Last Updated: ${update}`;
