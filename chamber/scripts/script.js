f7a9e66448d00fc4496000243bd933d385214578
adcostanzi <andres.costanzi@outlook.com>
2023-06-03 03:03:17 -0500

    discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -29,23 +29,3 @@ document.querySelector("#year").textContent = year;
 document.querySelector("#last-modified").textContent = `Last modified ${document.lastModified}`;
 
 
-// Toggle banner on top if it's Monday or Tuesday
-
-let banner = document.querySelector(".banner");
-
-
-// for test purposes: "May 15, 2023 23:15:00"
-
-let day = new Date().getDay();
-
-
-if (day == 1 || day == 2)
-{
-    banner.classList.toggle("toggleOn");
-} 
-
-const closeBtn = document.querySelector("#close-button");
-
-closeBtn.addEventListener("click", () => {
-    banner.style.display = "none";
-});
