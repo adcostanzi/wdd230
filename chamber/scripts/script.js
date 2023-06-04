af11dce2ea52806557be015a59f9dd45e6710331
adcostanzi <andres.costanzi@outlook.com>
2023-06-03 22:51:58 -0500

    user visit tracker

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -28,4 +28,24 @@ document.querySelector("#year").textContent = year;
 
 document.querySelector("#last-modified").textContent = `Last modified ${document.lastModified}`;
 
+// lastLog is the <span> that will show results
+const lastLog = document.querySelector("#last-login");
+const msToDays = 86400000;
 
+// call from localStorage the last-visit item, get current time
+let lastVisit = Number(localStorage.getItem("last-visit"));
+let todaysVisit = Date.now();
+
+// get difference between last visit and current time, get number of days and round it up
+let days = Math.round((todaysVisit - lastVisit) / msToDays);
+
+if(days === 0){
+    // If there is no days it means the used has already visited it today
+    lastLog.textContent = `You have already visited our site today =)`;
+
+} else {
+    // Display number of days since last visit
+    lastLog.textContent = `Number of days since your last visit: ${days}`;
+}
+// Store in localStorage the current time to be used as last-visit
+localStorage.setItem("last-visit", todaysVisit);
