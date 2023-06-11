0d0534955a9cd783b2f74756f3d2fddcc5f0e677
adcostanzi <andres.costanzi@outlook.com>
2023-06-10 21:02:22 -0500

    form completed

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,3 +1,4 @@
+// On click of Read more button open inline-modal
 const modal = document.querySelector("#modal-membership");
 
 const modalBtnOpen = document.querySelector("#modal-open");
@@ -12,3 +13,13 @@ modalBtnOpen.addEventListener("click", () =>{
 modalBtnClose.addEventListener("click", () => {
     modal.style.display = "none";
 });
+
+// get current date and time and apply it to hidden input
+const hiddenDate = document.querySelector("#todayDate");
+
+function getHiddenDate() {
+    let today = new Date();
+    hiddenDate.value = today;
+}
+
+getHiddenDate();
