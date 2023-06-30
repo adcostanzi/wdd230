26c5665ee95508a52844b6a666456e0c2e94e70d
adcostanzi <andres.costanzi@outlook.com>
2023-06-30 00:59:31 -0500

    modal script erased

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,19 +1,3 @@
-// On click of Read more button open inline-modal
-const modal = document.querySelector("#modal-membership");
-
-const modalBtnOpen = document.querySelector("#modal-open");
-
-const modalBtnClose = document.querySelector("#modal-close");
-
-modalBtnOpen.addEventListener("click", () =>{
-    modal.style.display = "block";
-});
-
-
-modalBtnClose.addEventListener("click", () => {
-    modal.style.display = "none";
-});
-
 // get current date and time and apply it to hidden input
 const hiddenDate = document.querySelector("#todayDate");
 
